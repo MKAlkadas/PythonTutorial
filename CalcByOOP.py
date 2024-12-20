c5f808675daa8f68491ab59e987d2394570119bd
MKMK <mk@gmail.com>
2024-12-20 16:49:58 +0300

    commit by MK

---
+++ CalcByOOP.py
@@ -0,0 +1,24 @@
+
+class Calculator:
+    def __init__(self, num1, num2):
+        self.num1 = num1
+        self.num2 = num2
+
+    def add(self):
+            return self.num1 + self.num2
+    
+    def sub(self):
+            return self.num1 - self.num2
+    def mul(self):
+            return self.num1 * self.num2
+
+    def div(self):
+            return self.num1 / self.num2
+    
+
+calc = Calculator(20,10)
+
+print(calc.add())
+print(calc.sub())
+print(calc.mul())
+
---
+++ _class_object.py
@@ -0,0 +1,16 @@
+class Car:
+    model = "Toyota"
+    color = "Red"
+    price = 35
+
+    def openDoor(self):
+        
+        print("python")
+        print(self)
+
+
+x = Car()
+
+
+x.openDoor()
+print(x)
