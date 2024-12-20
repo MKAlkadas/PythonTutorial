b4f3a8dcee5e0c5521ab4a8b2425fbd4a248a640
MKMK <mk@gmail.com>
2024-12-20 16:06:33 +0300

    Commit

---
+++ 3_variables.py
@@ -0,0 +1,38 @@
+# Create variables
+
+a = 1 
+b = 10
+
+# print variables
+
+print(a)
+print(b)
+
+# change variable
+x = 4       # x is of type int
+x = "change x" # x is now of type str
+print(x)
+
+# Casting
+
+x = str(3)    # x will be '3'
+y = int(3)    # y will be 3
+z = float(3)  # z will be 3.0
+
+# Get the Type
+x = 5
+y = "python"
+print(type(x))
+print(type(y))
+
+# Single or Double Quotes
+
+x = "python"
+# is the same as
+x = 'python'
+
+
+# Case-Sensitive
+a = 4
+A = "Python"
+#A will not overwrite a
